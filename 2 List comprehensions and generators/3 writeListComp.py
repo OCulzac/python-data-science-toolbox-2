d3c6a9ddc38ec3a32dee087083426e9b7a11fb53
oronc <oronculzac@gmail.com>
2019-04-24 09:48:52 -0400

    adds writing list comprehensions file

---
+++ 2 List comprehensions and generators/3 writeListComp.py
@@ -0,0 +1,5 @@
+""" Writing list comprehensions
+You now have all the knowledge necessary to begin writing list comprehensions! Your job in this exercise is to write a list comprehension that produces a list of the squares of the numbers ranging from 0 to 9. """
+
+# Create list comprehension: squares
+squares = [i*i for i in range(10)]
