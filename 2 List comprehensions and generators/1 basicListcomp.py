70d0d01427c176477031bf10d865a5dba71044ff
oronc <oronculzac@gmail.com>
2019-04-24 09:40:54 -0400

    adds chapter labels to folders, create new chapter adds basic list comprehension exercise file

---
+++ 2 List comprehensions and generators/1 basicListcomp.py
@@ -0,0 +1,11 @@
+""" Write a basic list comprehension
+In this exercise, you will practice what you've learned from the video about writing list comprehensions. You will write a list comprehension and identify the output that will be produced.
+
+The following list has been pre-loaded in the environment.
+
+doctor = ['house', 'cuddy', 'chase', 'thirteen', 'wilson']
+How would a list comprehension that produces a list of the first character of each string in doctor look like? Note that the list comprehension uses doc as the iterator variable. What will the output be? """
+
+doctor = ['house', 'cuddy', 'chase', 'thirteen', 'wilson']
+
+print ([doc[0] for doc in doctor])
